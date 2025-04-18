7e34abb0df16b5fefb8165b15d000e3a62bae7f2
bhuwan <bkspoudel@gmail.com>
2025-04-18 11:31:12 +0200

    create a folder for quiz and create Question class

---
+++ Question.java
@@ -0,0 +1,20 @@
+public class Question{
+    private int id;
+    private String question;
+    private String opt1;
+    private String opt2;
+    private String otp3;
+    private String opt4;
+    private String answer;
+
+    public Question(int id, String question, String opt1, String opt2, String opt3, String opt4, String answer){
+     this.id = id;
+     this.question = question;
+     this.opt1 = opt1;
+     this.opt1 = opt1;
+     this.opt1 = opt1;
+     this.opt1 = opt1;
+     this.answer = answer;
+
+    }
+}
