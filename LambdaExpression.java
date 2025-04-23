780472ccd3723b085af03bac6f6981910c77c6b9
bhuwan <bkspoudel@gmail.com>
2025-04-23 14:35:11 +0200

    revision how array method works

--- LambdaExpression.java
+++ LambdaExpression.java
@@ -5,7 +5,15 @@ interface A {
 
 public class LambdaExpression {
    public static void main(String[] args){
-    A obj = () -> System.out.println("I am in A");
-       obj.show();
-   }
+//     A obj = () -> System.out.println("I am in A");
+//        obj.show();
+//    }
+    A obj = new A() {
+        public void show(){
+            System.out.println("I am in new A....");
+        }
+    };
+    obj.show();
+   
+    }
 }
